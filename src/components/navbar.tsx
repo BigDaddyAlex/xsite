0ffdbe25dd2359ecbad282f213736bc39b46f0a8
Alex Sun <sqcalexander@gmail.com>
2023-07-25 23:12:09 -0700

    s

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -6,21 +6,13 @@ import React from 'react';
 
 
 export const Navbar = (props: any) => {
-  let navigate = useNavigate();
-
-  const ModalHandler = () => {
-    navigate("auth")
-  }
-
-
-  
-
   return (
     <div>
       <nav className="navbar bg-black p-2 align-items-end border-bottom">
         <NavLink className="navbar-brand" to="/">
           <img className="img-fluid " style={{ height: 35 }} src="/logo-transparent-bg.png" />
         </NavLink>
+        <h3 className="text-white">Free California driving license test questions and answers</h3>
         
       </nav>
     </div>
